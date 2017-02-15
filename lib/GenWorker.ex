bbf701dfc8f2d7aca32b29e6870de1951704e863
Eduardo Luz <luz.eduardo@gmail.com>
2017-02-15 07:59:35 -0200

    handle call

--- lib/GenWorker.ex
+++ lib/GenWorker.ex
@@ -8,4 +8,21 @@ defmodule Learn.GenWorker do
   def init(:ok) do
     {:ok, %{}}
   end
+
+  #Client API
+  def get_temperature(pid, location) do
+    GenServer.call(pid, {:location, location})
+  end
+
+  #Server API
+  def handle_call({:location, location}, _from, state) do
+    case temperature_of(location) do
+      {:ok, temp} ->
+          new_state = update_state(state, location)
+          {:reply, "#{temp}", new_state}
+      _ ->
+          {:reply, :error, state}
+    end
+  end
+
 end
