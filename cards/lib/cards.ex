108ef7c52d1d99f6a575d29326f4179cb382fd36
Eduardo Luz <luz.eduardo@gmail.com>
2017-05-13 14:33:58 -0300

    start

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,20 +1,4 @@
 defmodule Cards do
-  @moduledoc """
-  Documentation for Cards.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Cards.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
 
   def create_deck do
     ["Ace", "Two", "Three","Four","Five"]
