786344a5b192bb3fb18d239a26a299991edbe91b
Eduardo Luz <luz.eduardo@gmail.com>
2017-05-14 12:13:06 -0300

    stage 1

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,7 +1,14 @@
 defmodule Cards do
 
   def create_deck do
-    ["Ace", "Two", "Three","Four","Five"]
+    values = ["Ace", "Two", "Three","Four","Five"]
+    suits = ["Spades","Clubs","Hearts","Diamonds"]
+
+    #instead of using List.flatten() with a for inside for you can use this kind of comprehension
+    #that results in the same way 
+    for suit <- suits, value <- values  do
+        "#{value} of #{suit}"
+    end
   end
 
   def shuffle(deck) do
