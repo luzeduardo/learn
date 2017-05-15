d49572665431a9b348b155b01d548dbef4ea1f84
Eduardo Luz <luz.eduardo@gmail.com>
2017-05-15 00:02:34 -0300

    file system interaction and pattern matching instead of if statement

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -5,7 +5,7 @@ defmodule Cards do
     suits = ["Spades","Clubs","Hearts","Diamonds"]
 
     #instead of using List.flatten() with a for inside for you can use this kind of comprehension
-    #that results in the same way 
+    #that results in the same way
     for suit <- suits, value <- values  do
         "#{value} of #{suit}"
     end
@@ -14,4 +14,22 @@ defmodule Cards do
   def shuffle(deck) do
     Enum.shuffle(deck)
   end
+
+  def deal(deal, hand_size) do
+    Enum.split(deal, hand_size)
+  end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    {status, binary_data} = File.read(filename)
+    case status do
+      :ok -> :erlang.binary_to_term binary_data
+      :error -> "File does not exist"
+    end
+  end
+
 end
