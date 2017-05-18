f299ce98934854249514b4ff7aaa0c9d04847f7c
Eduardo Luz <luz.eduardo@gmail.com>
2017-05-18 08:05:58 -0300

    flattening list

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -17,14 +17,24 @@ defmodule Identicon do
     and then chunk the hex data that is a list
     and then map this row with 3 items to mount the mirrored row
     ex.: [1,2,3,4,5,6]
-    After Enum.chunk ->  [[1,2,3],[4,5,6]]
+    After Enum.chunk ->  [[1,2,3],[4,5,6] ...]
     After Enum.map(&mirror_row/1) [[1,2,3,2,1],[4,5,6,5,4]]
+    After List.flatten -> [1,2,3,2,1,4,5,6,5,4]
+    After Enum.with_index -> [{1,0},{2,1},{3,2},{2,3},{1,3} ...]
   """
   def build_grid(%Identicon.Image{hex: hex} = image) do
-    hex
+    grid = hex
     |> Enum.chunk(3)
-    #this will map every item in the list and use a reference of mirror_row that receives 1 parameter 
+    #this will map every item in the list and use a reference of mirror_row that receives 1 parameter
     |> Enum.map(&mirror_row/1)
+    #creating an unique list
+    |> List.flatten
+    #we need to create an index for each value in the list to create the grid for identicon
+    # it will return for each value inside the list a tuple containing the {value of the list, index in the list}
+    |> Enum.with_index
+    #get all avaiable data from the struct inside image and change the grid value using the suggar verticalbar operator
+    #returning it
+    %Identicon.Image{image | grid: grid}
   end
 
   def mirror_row(row) do
