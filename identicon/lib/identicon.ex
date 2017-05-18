3629e3a5d020d9fa1cbd435aa7d566ed1c586f1a
Eduardo Luz <luz.eduardo@gmail.com>
2017-05-17 22:28:39 -0300

    mirroring list

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -15,15 +15,21 @@ defmodule Identicon do
   @doc """
     This will pattern match the parameter against Identicon Image struct to decide it is ok
     and then chunk the hex data that is a list
+    and then map this row with 3 items to mount the mirrored row
+    ex.: [1,2,3,4,5,6]
+    After Enum.chunk ->  [[1,2,3],[4,5,6]]
+    After Enum.map(&mirror_row/1) [[1,2,3,2,1],[4,5,6,5,4]]
   """
   def build_grid(%Identicon.Image{hex: hex} = image) do
     hex
     |> Enum.chunk(3)
-    # |> mirrow_row
+    #this will map every item in the list and use a reference of mirror_row that receives 1 parameter 
+    |> Enum.map(&mirror_row/1)
   end
 
-  def mirror_row do
-
+  def mirror_row(row) do
+    [first, second, _tail] = row
+    row ++ [second, first]
   end
 
   @doc """
